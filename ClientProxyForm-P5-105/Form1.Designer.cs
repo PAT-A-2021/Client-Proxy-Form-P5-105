6eaa622784f2b45471fa2ffb429bca9f1beb5d27
Pahlevi Pasha <pahlevi.pasha09@gmail.com>
2021-11-11 17:58:22 +0700

    Add project files.

---
+++ ClientProxyForm-P5-105/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace ClientProxyForm_P5_105
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnhasil = new System.Windows.Forms.Button();
+            this.angka1 = new System.Windows.Forms.Label();
+            this.angka2 = new System.Windows.Forms.Label();
+            this.hasiljumlah = new System.Windows.Forms.Label();
+            this.hasilkurang = new System.Windows.Forms.Label();
+            this.hasilkali = new System.Windows.Forms.Label();
+            this.hasilbagi = new System.Windows.Forms.Label();
+            this.labeljumlah = new System.Windows.Forms.Label();
+            this.labelkurang = new System.Windows.Forms.Label();
+            this.labelkali = new System.Windows.Forms.Label();
+            this.labelbagi = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnhasil
+            // 
+            this.btnhasil.Location = new System.Drawing.Point(294, 286);
+            this.btnhasil.Name = "btnhasil";
+            this.btnhasil.Size = new System.Drawing.Size(75, 23);
+            this.btnhasil.TabIndex = 0;
+            this.btnhasil.Text = "Hasil";
+            this.btnhasil.UseVisualStyleBackColor = true;
+            this.btnhasil.Click += new System.EventHandler(this.btnhasil_Click);
+            // 
+            // angka1
+            // 
+            this.angka1.AutoSize = true;
+            this.angka1.Location = new System.Drawing.Point(135, 132);
+            this.angka1.Name = "angka1";
+            this.angka1.Size = new System.Drawing.Size(44, 13);
+            this.angka1.TabIndex = 1;
+            this.angka1.Text = "Angka1";
+            // 
+            // angka2
+            // 
+            this.angka2.AutoSize = true;
+            this.angka2.Location = new System.Drawing.Point(135, 187);
+            this.angka2.Name = "angka2";
+            this.angka2.Size = new System.Drawing.Size(44, 13);
+            this.angka2.TabIndex = 2;
+            this.angka2.Text = "Angka2";
+            // 
+            // hasiljumlah
+            // 
+            this.hasiljumlah.AutoSize = true;
+            this.hasiljumlah.Location = new System.Drawing.Point(489, 85);
+            this.hasiljumlah.Name = "hasiljumlah";
+            this.hasiljumlah.Size = new System.Drawing.Size(94, 13);
+            this.hasiljumlah.TabIndex = 3;
+            this.hasiljumlah.Text = "Hasil Penjumlahan";
+            // 
+            // hasilkurang
+            // 
+            this.hasilkurang.AutoSize = true;
+            this.hasilkurang.Location = new System.Drawing.Point(486, 132);
+            this.hasilkurang.Name = "hasilkurang";
+            this.hasilkurang.Size = new System.Drawing.Size(97, 13);
+            this.hasilkurang.TabIndex = 4;
+            this.hasilkurang.Text = "Hasil Pengurangan";
+            // 
+            // hasilkali
+            // 
+            this.hasilkali.AutoSize = true;
+            this.hasilkali.Location = new System.Drawing.Point(486, 187);
+            this.hasilkali.Name = "hasilkali";
+            this.hasilkali.Size = new System.Drawing.Size(77, 13);
+            this.hasilkali.TabIndex = 5;
+            this.hasilkali.Text = "Hasil Perkalian";
+            // 
+            // hasilbagi
+            // 
+            this.hasilbagi.AutoSize = true;
+            this.hasilbagi.Location = new System.Drawing.Point(486, 241);
+            this.hasilbagi.Name = "hasilbagi";
+            this.hasilbagi.Size = new System.Drawing.Size(86, 13);
+            this.hasilbagi.TabIndex = 6;
+            this.hasilbagi.Text = "Hasil Pembagian";
+            // 
+            // labeljumlah
+            // 
+            this.labeljumlah.AutoSize = true;
+            this.labeljumlah.Location = new System.Drawing.Point(628, 85);
+            this.labeljumlah.Name = "labeljumlah";
+            this.labeljumlah.Size = new System.Drawing.Size(10, 13);
+            this.labeljumlah.TabIndex = 7;
+            this.labeljumlah.Text = " ";
+            // 
+            // labelkurang
+            // 
+            this.labelkurang.AutoSize = true;
+            this.labelkurang.Location = new System.Drawing.Point(628, 132);
+            this.labelkurang.Name = "labelkurang";
+            this.labelkurang.Size = new System.Drawing.Size(10, 13);
+            this.labelkurang.TabIndex = 8;
+            this.labelkurang.Text = " ";
+            // 
+            // labelkali
+            // 
+            this.labelkali.AutoSize = true;
+            this.labelkali.Location = new System.Drawing.Point(628, 187);
+            this.labelkali.Name = "labelkali";
+            this.labelkali.Size = new System.Drawing.Size(10, 13);
+            this.labelkali.TabIndex = 9;
+            this.labelkali.Text = " ";
+            // 
+            // labelbagi
+            // 
+            this.labelbagi.AutoSize = true;
+            this.labelbagi.Location = new System.Drawing.Point(622, 241);
+            this.labelbagi.Name = "labelbagi";
+            this.labelbagi.Size = new System.Drawing.Size(10, 13);
+            this.labelbagi.TabIndex = 10;
+            this.labelbagi.Text = " ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(218, 124);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(221, 20);
+            this.textBox1.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(218, 184);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(221, 20);
+            this.textBox2.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelbagi);
+            this.Controls.Add(this.labelkali);
+            this.Controls.Add(this.labelkurang);
+            this.Controls.Add(this.labeljumlah);
+            this.Controls.Add(this.hasilbagi);
+            this.Controls.Add(this.hasilkali);
+            this.Controls.Add(this.hasilkurang);
+            this.Controls.Add(this.hasiljumlah);
+            this.Controls.Add(this.angka2);
+            this.Controls.Add(this.angka1);
+            this.Controls.Add(this.btnhasil);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnhasil;
+        private System.Windows.Forms.Label angka1;
+        private System.Windows.Forms.Label angka2;
+        private System.Windows.Forms.Label hasiljumlah;
+        private System.Windows.Forms.Label hasilkurang;
+        private System.Windows.Forms.Label hasilkali;
+        private System.Windows.Forms.Label hasilbagi;
+        private System.Windows.Forms.Label labeljumlah;
+        private System.Windows.Forms.Label labelkurang;
+        private System.Windows.Forms.Label labelkali;
+        private System.Windows.Forms.Label labelbagi;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ ClientProxyForm-P5-105/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientProxyForm_P5_105
+{
+    public partial class Form1 : Form
+    {
+
+        ServiceReference1.MatematikaClient obj = new ServiceReference1.MatematikaClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnhasil_Click(object sender, EventArgs e)
+        {
+            int angka1 = Convert.ToInt32(textBox1.Text);
+            int angka2 = Convert.ToInt32(textBox2.Text);
+
+            double hasiltambah = obj.Tambah(angka1, angka2);
+            double hasilkurang = obj.Kurang(angka1, angka2);
+            double hasilkali = obj.Kali(angka1, angka2);
+            double hasilbagi = obj.Bagi(angka1, angka2);
+
+            labeljumlah.Text = hasiltambah.ToString();
+            labelkurang.Text = hasilkurang.ToString();
+            labelkali.Text = hasilkali.ToString();
+            labelbagi.Text = hasilbagi.ToString();
+        }
+    }
+}
